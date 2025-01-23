4b902915b7805b3c1089db5c4ccdd0b034790997
vikasmaddheshiya-003 <137639526+vikasmaddheshiya-003@users.noreply.github.com>
2025-01-23 17:35:19 +0530

    Add files via upload

---
+++ DemoClass.java
@@ -0,0 +1,8 @@
+class DemoClass
+{
+  public static void main (String args[])
+{
+
+   System.out.print("hellow world");
+}
+}
---
+++ p1.java
@@ -0,0 +1,16 @@
+class p1
+
+{
+
+    public static void main(String [] arg)
+  {
+   
+
+     int mark=430;
+     int tatalMark=500;
+     double temp=430/500;
+     double percent=temp*100;
+     System.out.print("percent : "+ percent);
+
+   }
+}
