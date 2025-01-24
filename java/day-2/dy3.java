79800a225275cb04c62a7da045d77ca94778407a
vikasmaddheshiya-003 <137639526+vikasmaddheshiya-003@users.noreply.github.com>
2025-01-24 20:43:32 +0530

    Add files via upload

---
+++ java/day-2/dy3.java
@@ -0,0 +1,15 @@
+class day3
+{
+
+    public static void main(String [] arg)
+  {
+   
+
+     int mark=430;
+     int tatalMark=500;
+     double temp=430/500;
+     double percent=temp*100;
+     System.out.print("percent : "+ percent);
+
+   }
+}
