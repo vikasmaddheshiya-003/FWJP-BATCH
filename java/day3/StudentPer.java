a5c81bb637fea5228c8bd17651d0b03ccb8cf081
vikasmaddheshiya-003 <137639526+vikasmaddheshiya-003@users.noreply.github.com>
2025-01-24 20:08:52 +0530

    Add files via upload

---
+++ java/day3/StudentPer.java
@@ -0,0 +1,10 @@
+class StudentPer
+{
+  public static void main(String []argd)
+  {
+    float getMark=430f;
+    float totalMark=500f;
+    float percent=(getMark/totalMark)*100;
+    System.out.print("student percentage : "+ percent +" %");
+      }
+}
