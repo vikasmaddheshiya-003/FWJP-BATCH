3e9307620626e9f1e615140dcb6d782350876414
vikasmaddheshiya-003 <137639526+vikasmaddheshiya-003@users.noreply.github.com>
2025-01-24 21:41:39 +0530

    Add files via upload

---
+++ java/day-2/Q5_SquareDouble.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class Q5_SquareDouble
+{
+
+  
+   
+
+  public static void main(String args[])
+   {
+
+     Scanner sc=new Scanner(System.in);
+      double a=sc.nextDouble();
+      System.out.print("Square of "+a+ " : "+ (a*a));
+    }
+
+  
+}
