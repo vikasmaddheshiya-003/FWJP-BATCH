1729b1d8b1e832dc119e634ecf1d2052a021227f
vikasmaddheshiya-003 <137639526+vikasmaddheshiya-003@users.noreply.github.com>
2025-01-24 21:43:53 +0530

    Add files via upload

---
+++ java/day-2/Q4_IntSumDouble.java
@@ -0,0 +1,18 @@
+
+class Q4_IntSumDouble
+{
+
+  
+
+  public static void main(String args[])
+   {
+      int a=5;
+      double b=6.2;
+     // int sum=a+b;  // possible lossy conversion from double to int
+      double sum=a+b;
+
+    System.out.print(sum);
+    }
+
+  
+}
