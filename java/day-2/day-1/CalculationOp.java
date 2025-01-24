07a40c086c059f03cda078c4466365b865011436
vikasmaddheshiya-003 <137639526+vikasmaddheshiya-003@users.noreply.github.com>
2025-01-24 20:34:33 +0530

    Add files via upload

---
+++ java/day-2/day-1/CalculationOp.java
@@ -0,0 +1,16 @@
+class CalculationOp
+{
+  public static void main( String []args)
+  {
+     int a=10,b=20;
+     System.out.println("sum of "+a+" and "+b+" : "+(a+b));
+
+       System.out.println("sub of "+a+" and "+b+" : "+(a-b));
+
+        System.out.println("multiplication of "+a+" and "+b+" : "+(a*b));
+
+         System.out.println("division of "+a+" and "+b+" : "+(a/b));
+
+          System.out.println("mod of "+a+" and "+b+" : "+(a%b));
+  }
+}
